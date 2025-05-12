4624908736f1669cccda4a778cfb23834b95ab56
Sylla Yacouba <sylyac2000@gmail.com>
2025-05-12 11:56:04 +0000

    correcting test file

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("sljl");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
